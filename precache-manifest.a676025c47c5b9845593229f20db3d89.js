759ba351909815cba7203944659b26557fb2cee5
Alex Nazarenko <alexnaz8@gmail.com>
2020-01-30 12:49:55 +0200

    Updates

---
+++ precache-manifest.a676025c47c5b9845593229f20db3d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e8e2744a9d758980fda801c596efed",
+    "url": "/fun-monster-test/index.html"
+  },
+  {
+    "revision": "8505a2fc4d29a9993dc9",
+    "url": "/fun-monster-test/static/css/main.323c4a00.chunk.css"
+  },
+  {
+    "revision": "39ee3da454f31efe9302",
+    "url": "/fun-monster-test/static/js/2.f990e183.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fun-monster-test/static/js/2.f990e183.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8505a2fc4d29a9993dc9",
+    "url": "/fun-monster-test/static/js/main.91c413c0.chunk.js"
+  },
+  {
+    "revision": "ae781ce09d3c29d7b6a1",
+    "url": "/fun-monster-test/static/js/runtime-main.cdbd668f.js"
+  }
+]);
